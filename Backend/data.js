73d98e24e9344122358c8950f07305032c98474c
Carlos Barbosa <125832186+CarlosD-1699@users.noreply.github.com>
2023-03-14 17:49:36 -0500

    feat: Updated data and add new api to product id

--- Backend/data.js
+++ Backend/data.js
@@ -1,6 +1,7 @@
 const data = {
     products:[
         {
+            _id: '1',
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt1',
             category: 'Shirts',
@@ -13,24 +14,26 @@ const data = {
             description: 'sample product',
         },
         {
+            _id: '2',
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt2',
             category: 'Shirts',
             image: '/images/p1.jpg',
             price: 120,
-            countInStock: 10,
+            countInStock: 20,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
             description: 'sample product',
         },
         {
+            _id: '3',
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt3',
             category: 'Shirts',
             image: '/images/p2.png',
             price: 120,
-            countInStock: 10,
+            countInStock: 20,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
