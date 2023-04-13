a1055b7d844d72ddd63ec72f77c3f8b4e9fbeef8
Carlos Barbosa <125832186+CarlosD-1699@users.noreply.github.com>
2023-04-12 21:21:38 -0500

    fix: fixed headers sintax

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ app.get('/api/keys/paypal', (req, res) => {
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
   next();
 });
 
