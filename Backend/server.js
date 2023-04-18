b1e99630bd6b5beae3a92d449bf3464fa7bf4ebd
Carlos Barbosa <125832186+CarlosD-1699@users.noreply.github.com>
2023-04-18 13:03:28 -0500

    fix: fixed header allow methods

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.get('/api/keys/paypal', (req, res) => {
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials: true");
-  res.header("Access-Control-Allow-Methods: *");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, authorization");
   next();
 });
